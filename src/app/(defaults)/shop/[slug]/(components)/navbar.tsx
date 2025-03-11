560e2a3e4e76d67cfdaea99da04291392304ba56
tausifabid12 <tausifabid1294@gmail.com>
2025-03-11 15:46:49 +0600

    update

--- src/app/(defaults)/shop/[slug]/(components)/navbar.tsx
+++ src/app/(defaults)/shop/[slug]/(components)/navbar.tsx
@@ -23,7 +23,7 @@ export default function ShopNavbar({ removeFromCart, addToCart, getCartDetails, 
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 pt-0.5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
-                    <input className="ml-2 outline-none bg-transparent font-" type="text" name="search" id="search" placeholder="Search..." />
+                    {/* <input className="ml-2 outline-none bg-transparent font-" type="text" name="search" id="search" placeholder="Search..." /> */}
                 </div>
                 <ul className="flex items-center space-x-6">
 
